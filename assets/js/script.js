540ac9b3c3b914ed49af0b080f5a0eb6aca2f961
catherinecunningham <cfunk512@gmail.com>
2022-07-22 10:21:47 -0700

    add password length alerts

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,16 +4,12 @@ var alphabetUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var numbers = "0123456789";
 var specialCharacters = "&^*()%$#@!";
 var finalPassword = [];
-
-
 var passChoices = '';
 var generateBtn = document.querySelector("#generate");
 
 // FUNCTIONS
 function writePassword() {
   var password = generatePassword();
-
-  // connects to the button on the HTML page
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
@@ -21,61 +17,59 @@ function writePassword() {
 }
 
 function generatePassword() {
+  // prompt and confirm password length
     var length = window.prompt("Choose a number between 8 and 128 characters for your password.");
-    var wantLowercase = window.confirm("Do you want to include lowercase letters?");
-    var wantUppercase = window.confirm("Do you want to include uppercase letters?");
-    var wantNumbers = window.confirm("Do you want to include numbers?");
-    var wantSpecialChar = window.confirm("Do you want to include special characters?")
+
+    if (length >= 8 && length <= 128) {
+      // establish character selections for password
+      var wantLowercase = window.confirm("Do you want to include lowercase letters?");
+      var wantUppercase = window.confirm("Do you want to include uppercase letters?");
+      var wantNumbers = window.confirm("Do you want to include numbers?");
+      var wantSpecialChar = window.confirm("Do you want to include special characters?")
     
 
-    if (wantLowercase === true) {
-      passChoices = passChoices.concat(alphabetLower);
-    } if (wantUppercase === true) {
-      passChoices = passChoices.concat(alphabetUpper);
-    } if (wantNumbers === true) {
-      passChoices = passChoices.concat(numbers);
-    } if (wantSpecialChar === true) {
-      passChoices = passChoices.concat(specialCharacters);
+      if (wantLowercase === true) {
+        passChoices = passChoices.concat(alphabetLower);
+      } if (wantUppercase === true) {
+        passChoices = passChoices.concat(alphabetUpper);
+      } if (wantNumbers === true) {
+        passChoices = passChoices.concat(numbers);
+      } if (wantSpecialChar === true) {
+        passChoices = passChoices.concat(specialCharacters);
+      }
+      // if no character selections are made
+      else if (passChoices === '') {
+        window.alert("You must select at least one character type for your password. Please try again.");
+      }
+
+      // split string to prep for randomizer
+      passChoices = passChoices.split('');
+
+      for (i=0; i<length; i++) {
+        var character = passChoices[Math.floor(Math.random() * passChoices.length)];
+        finalPassword.push(character);
+      }
+
+      console.log(passChoices);
+
+      console.log(finalPassword.join(''));
+
+      var stringPass = finalPassword.join('')
+
+      // ouput of password on screen
+      return stringPass
     }
 
-    else if (passChoices === '') {
-      window.alert("You must select at least one character type for your password. Please try again.");
+    else if (length < 8) {
+      window.alert("You must select a password length between 8 and 128 characters. Please try again.")
     }
 
-    // split string to prep for randomizer
-    passChoices = passChoices.split('');
-
-    for (i=0; i<length; i++) {
-      var character = passChoices[Math.floor(Math.random() * passChoices.length)];
-      finalPassword.push(character);
+    else if (length > 128) {
+      window.alert("Your must select a password length between 8 and 128 characters. Please try again.")
     }
-
-    console.log(passChoices);
-
-    console.log(finalPassword.join(''));
-
-    var stringPass = finalPassword.join('')
-
-    return stringPass
-
 }
 
-
+// Run code through button click
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-// on button click, prompted to generate a password - window.prompt
-
-// prompted with password criteria
-  // prompted for the length of the password, 8 < password < 128
-
-  // choose character types
-    // uppercase, lowercase, numeric, and special characters
-
-// create variable alphabet = "all of the letters here" - uppercase and lowercase
-
-  // answer each prompt, input should be validated and at least one character type selected
-    // warning alert if at least one character type is NOT selected
-
-  // password generated that matches the criteria
-    // can be displayed in an alert OR written to the page
